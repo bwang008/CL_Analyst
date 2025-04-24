c1ac651bb82775aadbba01bf40941fd2a013518e
bwang008 <bwang008@gmail.com>
2025-04-24 03:13:14 -0700

    initial commit with data files

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ There will be a breakdown of the following
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('data/cl-5m_bk.csv',sep=';',parse_dates=[[0,1]],index_col=0)
+#data = pd.read_csv('data/cl-5m_bk.csv',sep=';',parse_dates=[[0,1]],index_col=0,dayfirst=True)
+data = pd.read_csv('data/test.csv',sep=';',header=None,index_col=None)
+        
+cols=['Date','Time','Open','High','Low','Close','Volume']
+data.columns = cols
+
+#Merge the date and time columns to form single DT column and assign it as the index/key
+
+
+breakpoint()
+#data.columns(cols)
+
 
 print(data.head())
